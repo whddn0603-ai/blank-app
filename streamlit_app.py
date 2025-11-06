789ca4472f9dff73803fa1eae40736e87876050b
whddn0603-ai <whddn0603@gmail.com>
2025-11-06 07:16:07 +0000

    테스트

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app Hi")
+st.title("🎈 김두형 바보")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
