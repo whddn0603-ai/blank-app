aa4b6d49c09daddf687c6de06870af695ed28a8f
whddn0603-ai <whddn0603@gmail.com>
2025-11-06 07:18:23 +0000

    테스트2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 김두형 바보")
+st.title("🎈 김두형 오늘 머리 감다 눈에 샴푸 들어감")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
